632fe173409344b6f437ff6b30baa9f6532a024c
georgiew-simeon <georgiew.simeon@gmail.com>
2024-11-11 15:30:02 +0000

    Additional changes

--- src/main/java/com/spring/app/project/springapp/rest/FunRestController.java
+++ src/main/java/com/spring/app/project/springapp/rest/FunRestController.java
@@ -1,14 +1,33 @@
 package com.spring.app.project.springapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
 
-    //    expose "/" endpoints returning "Hello World"
+    //    expose Endpoints
     @GetMapping("/")
     public String sayHello() {
         return "Hello world";
     }
+
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run a hard 5k!";
+    }
+
+    //    assign value from application.properties
+    @Value("${coach.name}")
+    private String coachName;
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "The coach is " + coachName + " and the team is " + teamName;
+    }
+
+    @Value("${team.name}")
+    private String teamName;
+
 }
